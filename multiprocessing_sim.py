004848726d6d05c3478128156278c13510b6388e
Brendan <bennett.brendan@yahoo.co.uk>
2024-02-29 23:09:33 +0000

    update multiprocessing to use get agent

--- multiprocessing_sim.py
+++ multiprocessing_sim.py
@@ -1,13 +1,12 @@
 from concurrent.futures import process
 from multiprocessing import Process, Queue
-from sim import XOAgentModel, Network, MCTS, save_training_data
+from sim import XOAgentModel, Network, MCTS, save_training_data, get_agent
 from torch import load
 
-N_PROCESSES = 4
+N_PROCESSES = 2
 GAMES_PER_SAVE = 4
-AGENT_REVISION = 3
 
-def worker(save_queue: Queue, agent: XOAgentModel, games_per_save):
+def worker(save_queue: Queue, games_per_save: int, agent: XOAgentModel):
     while True:
         list_training_data = []
         for i in range(games_per_save):
@@ -15,24 +14,21 @@ def worker(save_queue: Queue, agent: XOAgentModel, games_per_save):
             list_training_data.append(monte.self_play(agent, 200))
         save_queue.put(list_training_data)
 
-def saver(save_queue: Queue):
+def saver(save_queue: Queue, revision: str):
     while True:
         list_training_data = save_queue.get(block=True)
-        save_training_data(list_training_data, AGENT_REVISION)
+        save_training_data(list_training_data, revision)
 
 if __name__ == "__main__":
-    net = Network()
-    net.load_state_dict(load(f"models/trained_model_{AGENT_REVISION}"))
-    print("Loaded model weights")
-    agent = XOAgentModel(net)
-
     save_queue = Queue()
     processes: list[Process] = []
 
     for i in range(N_PROCESSES):
-        processes.append(Process(target=worker, args=(save_queue, agent, GAMES_PER_SAVE), daemon=True))
+        net = Network(hidden=128)
+        agent, revision = get_agent(network=net)
+        processes.append(Process(target=worker, args=(save_queue, GAMES_PER_SAVE, agent), daemon=True))
 
-    processes.append(Process(target=saver, args=(save_queue,), daemon=True))
+    processes.append(Process(target=saver, args=(save_queue, revision), daemon=True))
     
     for i in range(len(processes)):
         processes[i].start()
